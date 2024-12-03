d629866fb496523f783dd75696cad4ec7acbe2c3
Vercel Access <vercelaccess@gmail.com>
2024-12-03 21:05:33 +0500

    Git activity

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  //Created this line for git activity
+  //Deleted this line for git activity
   <React.StrictMode>
     <App />
   </React.StrictMode>,
